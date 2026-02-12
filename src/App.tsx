77de884449b134aedbfffa4466c0862820122dfd
Max Alexeev <max.alek@truelabel.io>
2026-02-12 18:30:40 +0300

    feat: supabase init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import './App.css'
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabase = createClient(
+//   import.meta.env.VITE_SUPABASE_URL,
+//   import.meta.env.VITE_SUPABASE_ANON_KEY
+// )
 
 function App() {
   return <div>App</div>
